7000f9eb6b86123075fd4ca79210bfb1c2bce6ce
peelz <peelz.dev@gmail.com>
2021-03-11 12:15:10 -0500

    Make use the generator for ourselves

--- VersionInfoGenerator.Generator/VersionInfoGenerator.cs
+++ VersionInfoGenerator.Generator/VersionInfoGenerator.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace VersionInfoGenerators
+namespace VersionInfoGenerators.Generator
 {
     [Generator]
     public class VersionInfoGenerator : ISourceGenerator
