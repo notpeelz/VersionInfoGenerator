08c68faa1246c02f427e9f5810347f9aecc5936e
peelz <peelz.dev@gmail.com>
2021-03-11 10:16:43 -0500

    Add test project

---
+++ VersionInfoGenerator.TestProject/TestProject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VersionInfoGenerator.TestProject
+{
+    public class TestProject
+    {
+        public static void Main()
+        {
+            Console.WriteLine("RootNamespace: " + VersionInfo.RootNamespace);
+            Console.WriteLine("Version: " + VersionInfo.Version);
+            Console.WriteLine("VersionPrerelease: " + VersionInfo.VersionPrerelease);
+            Console.WriteLine("VersionMetadata: " + VersionInfo.VersionMetadata);
+            Console.WriteLine("SemVer: " + VersionInfo.SemVer);
+            Console.WriteLine("GitRevShort: " + VersionInfo.GitRevShort);
+            Console.WriteLine("GitRevLong: " + VersionInfo.GitRevLong);
+            Console.WriteLine("GitBranch: " + VersionInfo.GitBranch);
+        }
+    }
+}
