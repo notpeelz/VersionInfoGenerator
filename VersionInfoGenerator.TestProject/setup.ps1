1c1895025cb30944da2892c4572ff165c5f6fc28
peelz <peelz.dev@gmail.com>
2025-06-10 19:09:22 -0400

    build: for the test project, always build before packing

--- VersionInfoGenerator.TestProject/setup.ps1
+++ VersionInfoGenerator.TestProject/setup.ps1
@@ -2,6 +2,7 @@ if (Test-Path "$PSScriptRoot/nuget-pkgs") {
   Remove-Item -Recurse -Force -ErrorAction Stop "$PSScriptRoot/nuget-pkgs"
 }
 
+dotnet build -c Release "$PSScriptRoot/../VersionInfoGenerator"
 dotnet pack "$PSScriptRoot/../VersionInfoGenerator" `
   -o "$PSScriptRoot/nuget-pkgs/src" `
   -c Release `
--- VersionInfoGenerator.TestProject/setup.sh
+++ VersionInfoGenerator.TestProject/setup.sh
@@ -6,6 +6,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 rm -rf "$DIR/nuget-pkgs"
 
+dotnet build -c Release "$DIR/../VersionInfoGenerator"
 dotnet pack "$DIR/../VersionInfoGenerator" \
   -o "$DIR/nuget-pkgs/src" \
   -c Release \
